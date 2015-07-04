e9116131e9d0832827ce1f7d66dd7619e1b51d22
Duality <201292@live.nl>
2015-07-05 01:21:03 +0200

    fixed bug that is in the pascal code that causes infinite loop instead of printing the names for 'variables'

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #define TAB "\t"
 int read = 0;
+int Lcount = 0;
 
-void Expression();
-void SkipWhite();
+char *str_malloc_refs[50];
+// str malloc ptr index
+int smpi = 0;
+
+void Block();
+void DoIf();
 
 void GetChar()
 {
@@ -91,6 +97,23 @@ void EmitLn(char *s)
     Emit(message);
 }
 
+char *NewLabel(void)
+{
+    char *string = (char *)malloc(sizeof(char)*4);
+    sprintf(string, "L%d", Lcount);
+    // increase label count.
+    Lcount++;
+    // keep reference for freeing later on.
+    str_malloc_refs[smpi] = string;
+    smpi++;
+    return string;
+}
+
+void PostLabel(char *s)
+{
+    printf("%s:\n", s);
+}
+
 void Init()
 {
     GetChar();
@@ -107,8 +130,35 @@ void Block()
 {
     while( read != 'e')
     {
-        Other();
+        switch(read)
+        {
+            case 'i':
+                DoIf();
+                break;
+            default:
+                Other();
+                break;
+        }
     }
+}
+
+void Condition()
+{
+    EmitLn("<condition>");
+}
+
+void DoIf()
+{
+    char *label;
+    Match('i');
+    label = NewLabel();
+    Condition();
+    char message[50];
+    sprintf(message, "BEQ %s", label);
+    EmitLn(message);
+    Block();
+    Match('e');
+    PostLabel(label);
 }
 
 void DoProgram()
@@ -121,9 +171,21 @@ void DoProgram()
     EmitLn("END");
 }
 
+void free_strings()
+{
+    int i;
+    for(i = 0;i < smpi;i++)
+    {
+        free(str_malloc_refs[i]);
+        str_malloc_refs[i] = NULL;
+    }
+}
+
 int main(void)
 {
     Init();
     DoProgram();
+    // free string created during the program.
+    free_strings();
     return 0;
 }
